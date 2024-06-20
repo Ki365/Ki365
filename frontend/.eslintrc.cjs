827b8686f39dfd5c2cbcbde0fe46f8f44a553456
OfficialKris <37947442+OfficialKris@users.noreply.github.com>
2024-06-19 22:14:06 -0600

    Updated extends in eslintrc

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,8 +3,10 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
