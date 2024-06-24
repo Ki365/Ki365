7a1c9f9e2e9d4067df46215df0cb710fc54689b0
OfficialKris <37947442+OfficialKris@users.noreply.github.com>
2024-06-23 18:33:32 -0600

    Added rollup-plugin-visualizer as dev dependency

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,14 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from "rollup-plugin-visualizer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+	react(),
+	visualizer()
+],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
