4a85ca0ee745961553a1f72a7b9ac94b6074e788
OfficialKris <37947442+OfficialKris@users.noreply.github.com>
2024-07-08 19:54:52 -0600

    Replaced loopback with localhost to fix WSL forwarding error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8100",
+        target: "http://localhost:8100",
         changeOrigin: true,
         ws: true,
       },
